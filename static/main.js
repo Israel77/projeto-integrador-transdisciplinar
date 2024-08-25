354fa9cc9cd466e7e8e01a39f611e0ae829c4606
Israel Batista <israel.silvino@gmail.com>
2024-08-25 12:17:56 -0300

    feat: Implementação de tags e movimentação de tarefas

--- static/main.js
+++ static/main.js
@@ -1,59 +1,181 @@
 (function () {
-    var quadroDiv = document.getElementById("quadro-kanban");
     var resp = {
         "idQuadro": 1,
         "titulo": "Nome do quadro",
+        "descricao": "Descrição do quadro",
         "colunas": [
             {
-                "idEstado": 20,
+                "idEstado": 1,
                 "nomeEstado": "Backlog",
                 "ordemEstado": 1,
                 "tarefas": [
                     {
                         "id": 1,
-                        "idEstado": 20,
+                        "idEstado": 1,
                         "titulo": "Título da tarefa",
                         "descricao": "Descrição da tarefa",
-                        tags: []
+                        tags: ["Frontend", "Backend"]
+                    },
+                    {
+                        "id": 2,
+                        "idEstado": 1,
+                        "titulo": "Título da tarefa",
+                        "descricao": "Uma tarefa razoavelmente longa",
+                        tags: ["Backend"]
                     }
                 ]
-            }
+            },
+            {
+                "idEstado": 2,
+                "nomeEstado": "Em andamento",
+                "ordemEstado": 2,
+                "tarefas": [
+                    {
+                        "id": 3,
+                        "idEstado": 3,
+                        "titulo": "Título da tarefa",
+                        "descricao": "Tarefa super longa que vai me tomar muito tempo",
+                        tags: ["Backend", "Performance"]
+                    }
+                ]
+            },
+            {
+                "idEstado": 3,
+                "nomeEstado": "Concluída",
+                "ordemEstado": 3,
+                "tarefas": [
+                    {
+                        "id": 4,
+                        "idEstado": 3,
+                        "titulo": "Título da tarefa",
+                        "descricao": "Descrição da tarefa",
+                        tags: ["Backend"]
+                    }
+                ]
+            },
         ]
     };
+    criarQuadro(resp);
+})();
+function criarQuadro(resp) {
+    var quadroDiv = document.getElementById("quadro-kanban");
+    // Criação
+    var tituloQuadro = document.createElement("h1");
+    var descricaoQuadro = document.createElement("p");
+    var colunaContainer = document.createElement("div");
+    // Estilo
+    colunaContainer.classList.add("coluna-container");
+    // Dados
+    tituloQuadro.innerText = resp.titulo;
+    descricaoQuadro.innerText = resp.descricao;
+    // Inserção
+    quadroDiv.appendChild(tituloQuadro);
+    quadroDiv.appendChild(descricaoQuadro);
+    quadroDiv.appendChild(colunaContainer);
     for (var _i = 0, _a = resp.colunas; _i < _a.length; _i++) {
         var estado = _a[_i];
-        criarColuna(quadroDiv, estado);
+        criarColuna(colunaContainer, estado);
     }
-})();
-function criarColuna(quadroDiv, estado) {
+}
+function criarColuna(root, estado) {
     // Criação
-    var estadoColuna = document.createElement("div");
-    // Inserção
-    quadroDiv.appendChild(estadoColuna);
+    var colunaEstado = document.createElement("div");
+    var colunaHeader = document.createElement("div");
+    var tituloColuna = document.createElement("h2");
+    var btnAdicionarTarefa = document.createElement("button");
     // Estilo
-    estadoColuna.classList.add("coluna-kanban");
+    colunaEstado.addEventListener("drop", drop);
+    colunaEstado.addEventListener("dragover", dragover);
+    colunaEstado.classList.add("coluna-kanban");
+    tituloColuna.classList.add("titulo-coluna");
+    colunaHeader.classList.add("header-coluna");
     // Dados
+    tituloColuna.innerText = estado.nomeEstado;
+    btnAdicionarTarefa.innerText = "+";
+    colunaEstado.setAttribute("data-id-estado", estado.idEstado.toString());
+    // Inserção
+    root.appendChild(colunaEstado);
+    colunaEstado.appendChild(colunaHeader);
+    colunaHeader.appendChild(btnAdicionarTarefa);
+    colunaHeader.appendChild(tituloColuna);
     for (var _i = 0, _a = estado.tarefas; _i < _a.length; _i++) {
         var tarefa = _a[_i];
-        var cardTarefa = criarCardTarefa(estadoColuna, tarefa);
+        criarCardTarefa(colunaEstado, tarefa);
     }
-    return estadoColuna;
+    return colunaEstado;
 }
-function criarCardTarefa(colunaEstado, tarefa) {
+function criarCardTarefa(root, tarefa) {
     // Criação
     var tarefaDiv = document.createElement("div");
     var pTitulo = document.createElement("p");
     var pDescricao = document.createElement("p");
+    var secaoTags = document.createElement("div");
     // Inserção
+    root.appendChild(tarefaDiv);
     tarefaDiv.appendChild(pTitulo);
     tarefaDiv.appendChild(pDescricao);
-    colunaEstado.appendChild(tarefaDiv);
+    tarefaDiv.appendChild(secaoTags);
     // Estilos
+    tarefaDiv.setAttribute("draggable", "true");
+    tarefaDiv.addEventListener("dragstart", drag);
     tarefaDiv.classList.add("card");
     pTitulo.classList.add("titulo-card");
     pDescricao.classList.add("descricao-card");
+    secaoTags.classList.add("tag-container");
     // Dados
+    tarefaDiv.setAttribute("data-id-tarefa", tarefa.id.toString());
     pTitulo.innerText = tarefa.titulo;
     pDescricao.innerText = tarefa.descricao;
+    for (var _i = 0, _a = tarefa.tags; _i < _a.length; _i++) {
+        var tag = _a[_i];
+        criarTag(secaoTags, tag);
+    }
     return tarefaDiv;
+}
+function criarTag(root, tag) {
+    // Criação
+    var tagElement = document.createElement("span");
+    // Inserção
+    root.appendChild(tagElement);
+    // Estilo
+    tagElement.classList.add("tag");
+    // Dados
+    tagElement.innerText = tag;
+    return tagElement;
+}
+function dragover(ev) {
+    ev.preventDefault();
+}
+function drag(ev) {
+    var _a, _b;
+    var tarefaId = (_a = ev.target) === null || _a === void 0 ? void 0 : _a.getAttribute("data-id-tarefa");
+    if (tarefaId)
+        (_b = ev.dataTransfer) === null || _b === void 0 ? void 0 : _b.setData("text", tarefaId);
+}
+function drop(ev) {
+    try {
+        ev.preventDefault();
+    }
+    catch (error) { }
+    if (ev.dataTransfer) {
+        var data = ev.dataTransfer.getData("text");
+        var card = document.querySelector("[data-id-tarefa=\"".concat(data, "\"]"));
+        if (card != null) {
+            mudarColuna(ev.target, card);
+        }
+    }
+}
+function mudarColuna(novaColuna, tarefa) {
+    // Apenas insere a tarefa se o alvo for uma coluna
+    while (!novaColuna.classList.contains("coluna-kanban")) {
+        if (novaColuna.parentElement) {
+            // Se não for uma coluna, tenta o elemento pai
+            novaColuna = novaColuna.parentElement;
+        }
+        else {
+            // Se o alvo não está contido em uma coluna, não faz nada
+            return;
+        }
+    }
+    novaColuna.appendChild(tarefa);
 }
--- static/main.ts
+++ static/main.ts
@@ -1,6 +1,7 @@
 type Quadro = {
     "idQuadro": number,
     "titulo": string,
+    "descricao": string,
     "colunas": Estado[]
 }
 
@@ -16,78 +17,214 @@ type Tarefa = {
     "idEstado": number,
     "titulo": string,
     "descricao": string,
-    "tags": string[]
+    "tags": Tag[]
 }
 
-(() => {
-    let quadroDiv = document.getElementById("quadro-kanban") as HTMLDivElement;
+type Tag = string;
 
+(() => {
     let resp: Quadro = {
         "idQuadro": 1,
         "titulo": "Nome do quadro",
+        "descricao": "Descrição do quadro",
         "colunas": [
             {
-                "idEstado": 20,
+                "idEstado": 1,
                 "nomeEstado": "Backlog",
                 "ordemEstado": 1,
                 "tarefas": [
                     {
                         "id": 1,
-                        "idEstado": 20,
+                        "idEstado": 1,
                         "titulo": "Título da tarefa",
                         "descricao": "Descrição da tarefa",
-                        tags: []
+                        tags: ["Frontend", "Backend"]
+                    },
+                    {
+                        "id": 2,
+                        "idEstado": 1,
+                        "titulo": "Título da tarefa",
+                        "descricao": "Uma tarefa razoavelmente longa",
+                        tags: ["Backend"]
                     }
                 ]
-            }
+            },
+            {
+                "idEstado": 2,
+                "nomeEstado": "Em andamento",
+                "ordemEstado": 2,
+                "tarefas": [
+                    {
+                        "id": 3,
+                        "idEstado": 3,
+                        "titulo": "Título da tarefa",
+                        "descricao": "Tarefa super longa que vai me tomar muito tempo",
+                        tags: ["Backend", "Performance"]
+                    }
+                ]
+            },
+            {
+                "idEstado": 3,
+                "nomeEstado": "Concluída",
+                "ordemEstado": 3,
+                "tarefas": [
+                    {
+                        "id": 4,
+                        "idEstado": 3,
+                        "titulo": "Título da tarefa",
+                        "descricao": "Descrição da tarefa",
+                        tags: ["Backend"]
+                    }
+                ]
+            },
         ]
     }
 
-    for (let estado of resp.colunas) {
-        criarColuna(quadroDiv, estado);
-    }
-
-
+    criarQuadro(resp);
 })()
 
-function criarColuna(quadroDiv: HTMLDivElement, estado: Estado): HTMLDivElement {
-    // Criação
-    let estadoColuna = document.createElement("div");
+function criarQuadro(resp: Quadro) {
+    let quadroDiv = document.getElementById("quadro-kanban") as HTMLDivElement;
 
-    // Inserção
-    quadroDiv.appendChild(estadoColuna);
-    for (let tarefa of estado.tarefas) {
-        let cardTarefa = criarCardTarefa(estadoColuna, tarefa);
-    }
+    // Criação
+    let tituloQuadro = document.createElement("h1");
+    let descricaoQuadro = document.createElement("p");
+    let colunaContainer = document.createElement("div");
 
     // Estilo
-    estadoColuna.classList.add("coluna-kanban")
+    colunaContainer.classList.add("coluna-container");
 
     // Dados
-    return estadoColuna;
+    tituloQuadro.innerText = resp.titulo;
+    descricaoQuadro.innerText = resp.descricao;
+
+    // Inserção
+    quadroDiv.appendChild(tituloQuadro);
+    quadroDiv.appendChild(descricaoQuadro);
+    quadroDiv.appendChild(colunaContainer);
+    for (let estado of resp.colunas) {
+        criarColuna(colunaContainer, estado);
+    }
 }
 
-function criarCardTarefa(colunaEstado: HTMLDivElement, tarefa: Tarefa): HTMLDivElement {
+function criarColuna(root: HTMLElement, estado: Estado): HTMLDivElement {
+    // Criação
+    let colunaEstado = document.createElement("div");
+    let colunaHeader = document.createElement("div");
+    let tituloColuna = document.createElement("h2");
+    let btnAdicionarTarefa = document.createElement("button");
+
+    // Estilo
+    colunaEstado.addEventListener("drop", drop)
+    colunaEstado.addEventListener("dragover", dragover)
+    colunaEstado.classList.add("coluna-kanban")
+    tituloColuna.classList.add("titulo-coluna")
+    colunaHeader.classList.add("header-coluna")
+
+    // Dados
+    tituloColuna.innerText = estado.nomeEstado;
+    btnAdicionarTarefa.innerText = "+";
+    colunaEstado.setAttribute("data-id-estado", estado.idEstado.toString());
+
+    // Inserção
+    root.appendChild(colunaEstado);
+
+    colunaEstado.appendChild(colunaHeader);
+    colunaHeader.appendChild(btnAdicionarTarefa);
+    colunaHeader.appendChild(tituloColuna);
+
+    for (let tarefa of estado.tarefas) {
+        criarCardTarefa(colunaEstado, tarefa);
+    }
+
+    return colunaEstado;
+}
+
+function criarCardTarefa(root: HTMLElement, tarefa: Tarefa): HTMLDivElement {
     // Criação
     let tarefaDiv = document.createElement("div");
     let pTitulo = document.createElement("p");
     let pDescricao = document.createElement("p");
+    let secaoTags = document.createElement("div");
 
     // Inserção
+    root.appendChild(tarefaDiv);
     tarefaDiv.appendChild(pTitulo);
     tarefaDiv.appendChild(pDescricao);
-    colunaEstado.appendChild(tarefaDiv)
+    tarefaDiv.appendChild(secaoTags)
+
 
     // Estilos
+    tarefaDiv.setAttribute("draggable", "true")
+    tarefaDiv.addEventListener("dragstart", drag)
     tarefaDiv.classList.add("card");
     pTitulo.classList.add("titulo-card");
     pDescricao.classList.add("descricao-card");
+    secaoTags.classList.add("tag-container");
 
     // Dados
+    tarefaDiv.setAttribute("data-id-tarefa", tarefa.id.toString());
     pTitulo.innerText = tarefa.titulo;
     pDescricao.innerText = tarefa.descricao;
+    for (let tag of tarefa.tags) {
+        criarTag(secaoTags, tag);
+    }
 
     return tarefaDiv;
 }
 
+function criarTag(root: HTMLElement, tag: Tag) {
+    // Criação
+    let tagElement = document.createElement("span");
 
+    // Inserção
+    root.appendChild(tagElement);
+
+    // Estilo
+    tagElement.classList.add("tag")
+
+    // Dados
+    tagElement.innerText = tag;
+
+    return tagElement
+}
+
+function dragover(ev: Event) {
+    ev.preventDefault();
+}
+
+function drag(ev: DragEvent) {
+    let tarefaId = (ev.target as HTMLElement)?.getAttribute("data-id-tarefa");
+    if (tarefaId)
+        ev.dataTransfer?.setData("text", tarefaId);
+}
+
+function drop(ev: DragEvent) {
+    try {
+        ev.preventDefault();
+    }
+    catch (error) { }
+
+    if (ev.dataTransfer) {
+        let data = ev.dataTransfer.getData("text");
+        let card = document.querySelector(`[data-id-tarefa="${data}"]`);
+
+        if (card != null) {
+            mudarColuna(ev.target as HTMLElement, card)
+        }
+    }
+}
+
+function mudarColuna(novaColuna: HTMLElement, tarefa: Element) {
+    // Apenas insere a tarefa se o alvo for uma coluna
+    while (!novaColuna.classList.contains("coluna-kanban")) {
+        if (novaColuna.parentElement) {
+            // Se não for uma coluna, tenta o elemento pai
+            novaColuna = novaColuna.parentElement
+        } else {
+            // Se o alvo não está contido em uma coluna, não faz nada
+            return;
+        }
+    }
+    novaColuna.appendChild(tarefa)
+}
