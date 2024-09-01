f752f523baad52a602e00a9f1ae077f998b24c5b
Israel Batista <israel.silvino@gmail.com>
2024-09-01 11:50:08 -0300

    feat: Inserir código de test no front-end para login

--- static/main.js
+++ static/main.js
@@ -55,6 +55,16 @@
     //         },
     //     ]
     // }
+    let login = await fetch("http://localhost:8080/api/v1/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "nome_usuario": "test",
+            "senha": "test"
+        })
+    });
     let resposta = (await fetch("http://localhost:8080/api/v1/quadro/1"));
     let corpoResposta = await resposta.json();
     criarQuadro(corpoResposta);
--- static/main.ts
+++ static/main.ts
@@ -80,6 +80,16 @@ type Tag = string;
     //     ]
     // }
 
+    let login = await fetch("http://localhost:8080/api/v1/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "nome_usuario": "test",
+            "senha": "test"
+        })
+    });
     let resposta = (await fetch("http://localhost:8080/api/v1/quadro/1"));
     let corpoResposta = await resposta.json() as Quadro;
     criarQuadro(corpoResposta);
