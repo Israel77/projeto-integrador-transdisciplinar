8ff4ff0eef675783904e71c6fa607308ec90ede7
Israel Batista <israel.silvino@gmail.com>
2024-11-02 19:20:42 -0300

    feat: Melhorar a UI para informar sobre validações na criação do usuário

--- static/pages/sign-up/main.ts
+++ static/pages/sign-up/main.ts
@@ -5,37 +5,96 @@ import { criarNovoUsuario } from "../../services/criarNovoUsuario.js";
     const emailUsuarioInput: HTMLInputElement = document.getElementById('email') as HTMLInputElement;
     const senhaInput: HTMLInputElement = document.getElementById('senha') as HTMLInputElement;
     const confirmarSenhaInput: HTMLInputElement = document.getElementById('confirmar-senha') as HTMLInputElement;
-    const pIndicadorSenha: HTMLParagraphElement = document.getElementById('indicador-senhas-diferentes') as HTMLParagraphElement;
-
     const formularioConta: HTMLFormElement = document.getElementById('formulario-criar-conta') as HTMLFormElement;
 
-    let senhaValida = false;
-    const usuarioValido = nomeUsuarioInput.value.length >= 3;
-    const emailValido = (emailUsuarioInput.value.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/) ?? [])
-        .length > 0;
 
-    confirmarSenhaInput.addEventListener("input", () => {
-        if (confirmarSenhaInput.value !== senhaInput.value) {
-            pIndicadorSenha.style.color = 'red';
-            pIndicadorSenha.classList.remove("hidden")
-            senhaValida = false;
-        } else {
-            pIndicadorSenha.classList.add("hidden")
-            senhaValida = true;
-        }
-    });
+    nomeUsuarioInput.addEventListener("input", reavaliarValidacoes);
+    emailUsuarioInput.addEventListener("input", reavaliarValidacoes);
+    senhaInput.addEventListener("input", reavaliarValidacoes);
+    confirmarSenhaInput.addEventListener("input", reavaliarValidacoes);
 
     formularioConta.addEventListener('submit', (ev: Event) => {
         ev.preventDefault();
 
-        if (!senhaValida || !usuarioValido || !emailValido) {
-            return;
-        }
-
         criarNovoUsuario(nomeUsuarioInput.value, emailUsuarioInput.value, senhaInput.value).then(resposta => {
             if (resposta.ok) {
                 window.location.href = '/criar-quadro';
+            } else {
+                console.error(resposta);
             }
         });
     });
 })()
+
+function reavaliarValidacoes(): boolean {
+    const nomeUsuarioInput: HTMLInputElement = document.getElementById('nome-usuario') as HTMLInputElement;
+    const emailUsuarioInput: HTMLInputElement = document.getElementById('email') as HTMLInputElement;
+    const senhaInput: HTMLInputElement = document.getElementById('senha') as HTMLInputElement;
+    const confirmarSenhaInput: HTMLInputElement = document.getElementById('confirmar-senha') as HTMLInputElement;
+    const botaoCriarConta: HTMLButtonElement = document.getElementById('botao-criar-conta') as HTMLButtonElement;
+
+    const pMensagemNomeUsuario = document.getElementById('msg-nome-usuario') as HTMLParagraphElement;
+    const pMensagemEmail = document.getElementById('msg-email') as HTMLParagraphElement;
+    const pMensagemSenha: HTMLParagraphElement = document.getElementById('msg-senha') as HTMLParagraphElement;
+    const pMensagemVerificadorSenha: HTMLParagraphElement = document.getElementById('msg-verificador-senha') as HTMLParagraphElement;
+
+    const usuarioValido = validarUsuario(nomeUsuarioInput.value);
+    const emailValido = validarEmail(emailUsuarioInput.value);
+    const senhasIguais = validarSenhasIguais(senhaInput.value, confirmarSenhaInput.value);
+    const senhaValida = validarSenha(senhaInput.value);
+
+    console.log(`Usuário válido: ${usuarioValido}`);
+
+    // Nome de usuário
+    if (!usuarioValido) {
+        pMensagemNomeUsuario.innerText = "Nome de usuário muito curto."
+        pMensagemNomeUsuario.classList.remove("hidden")
+    } else {
+        pMensagemNomeUsuario.classList.add("hidden")
+    }
+
+    // Email
+    if (!emailValido) {
+        pMensagemEmail.innerText = "Email inválido."
+        pMensagemEmail.classList.remove("hidden")
+    } else {
+        pMensagemEmail.classList.add("hidden")
+    }
+
+    // Senha
+    if (!senhaValida) {
+        pMensagemSenha.innerText = "Senha muito curta."
+        pMensagemSenha.classList.remove("hidden")
+    } else {
+        pMensagemSenha.classList.add("hidden")
+    }
+
+    if (!senhasIguais) {
+        pMensagemVerificadorSenha.innerText = "As senhas não coincidem."
+        pMensagemVerificadorSenha.classList.remove("hidden")
+    } else {
+        pMensagemVerificadorSenha.classList.add("hidden")
+    }
+
+    const resultado = usuarioValido && emailValido && senhaValida && senhasIguais;
+
+    botaoCriarConta.disabled = !resultado;
+    return resultado;
+}
+
+function validarUsuario(nomeUsuario: string): boolean {
+    return nomeUsuario.length > 0;
+}
+
+function validarEmail(email: string): boolean {
+    return (email.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/) ?? [])
+        .length > 0;
+}
+
+function validarSenha(senha: string): boolean {
+    return senha.length >= 8;
+}
+
+function validarSenhasIguais(senha: string, confirmacaoSenha: string): boolean {
+    return senha === confirmacaoSenha;
+}
