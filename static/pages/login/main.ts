637745db5b0c29bfd76371c7b8f7c33889afb62b
Israel Batista <israel.silvino@gmail.com>
2024-11-03 10:22:36 -0300

    feat: Exibir mensagem de erro em caso de falha no login

--- static/pages/login/main.ts
+++ static/pages/login/main.ts
@@ -1,7 +1,9 @@
+import { renderizarErro } from "../../components/Erro.js";
 import { appConfig } from "../../config.js";
 import { RequisicaoLogin } from "../../types/types";
 
 (() => {
+    const htmlBody: HTMLElement = document.getElementsByTagName('body')[0] as HTMLElement;
     const nomeUsuario: HTMLInputElement = document.getElementById('nome-usuario') as HTMLInputElement;
     const senha: HTMLInputElement = document.getElementById('senha') as HTMLInputElement;
 
@@ -28,6 +30,11 @@ import { RequisicaoLogin } from "../../types/types";
                 if (resposta.ok) {
                     console.log("Login realizado com sucesso!");
                     window.location.href = '/main';
+                } else {
+                    resposta.json().then(resposta => {
+                        renderizarErro(htmlBody, resposta);
+                        console.error(resposta.message);
+                    });
                 }
             })
             .catch(erro => console.error(erro));
