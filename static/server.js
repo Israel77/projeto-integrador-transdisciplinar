b68f8d2ca3bfc1bda9414aff7f87f0b35f16ad45
Israel Batista <israel.silvino@gmail.com>
2024-11-03 09:48:19 -0300

    feat: Criar páginas de 'recuperação de senha' e erro 404

--- static/server.js
+++ static/server.js
@@ -29,9 +29,23 @@ app.use("/main", express.static(path.join(__dirname, "pages/main")));
 app.use("/login", express.static(path.join(__dirname, "pages/login")));
 app.use("/sign-up", express.static(path.join(__dirname, "pages/sign-up")));
 app.use("/criar-quadro", express.static(path.join(__dirname, "pages/criar-quadro")));
+app.use("/recuperar-senha", express.static(path.join(__dirname, "pages/recuperar-senha")));
 app.use("/css", express.static(path.join(__dirname, "css")));
 app.use("/components", express.static(path.join(__dirname, "components")));
 app.use(express.static(__dirname));
+
+// Erro 404
+app.use((req, res) => {
+    res.status(404);
+
+    if (req.accepts("text/html")) {
+        res.sendFile(path.join(__dirname, "pages/404/index.html"));
+    } else if (req.accepts("application/json")) {
+        res.json({ error: "Not found" });
+    } else {
+        res.type("txt").send("Not found");
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
