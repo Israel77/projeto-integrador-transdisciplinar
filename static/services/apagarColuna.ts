42ab151e2b3edae67f843e17a4e7fc2e73cf35c8
Israel Batista <israel.silvino@gmail.com>
2024-11-16 16:45:52 -0300

    fix: Corrigir CSS para navegadores baseados em Chromium

--- static/services/apagarColuna.ts
+++ static/services/apagarColuna.ts
@@ -1,7 +1,7 @@
 import { appConfig } from "/config.js";
 
 export async function apagarColuna(idColuna: string) {
-    return await fetch(`${appConfig.baseApiUrl}/api/v1/coluna/apagar`, {
+    return fetch(`${appConfig.baseApiUrl}/api/v1/coluna/apagar`, {
         method: "DELETE",
         credentials: "include",
         headers: {
